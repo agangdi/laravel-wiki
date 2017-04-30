2dfc971549993d5a1fdaaceaa97bbcc904742e3e
just like before <agangdi@gmail.com>
2017-04-30 21:57:52 +0800

    git init

---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: johnShaw
+ * Date: 17/4/27
+ * Time: 上午12:07
+ */
+?>
+@if (count($errors) > 0)
+        <!-- Form Error List -->
+<div class="alert alert-danger">
+    <strong>Whoops! Something went wrong!</strong>
+
+    <br><br>
+
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: johnShaw
+ * Date: 17/4/26
+ * Time: 下午11:57
+ */
+// resources/views/layouts/app.blade.php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+
+        <!-- CSS And JavaScript -->
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+
+    @yield('content')
+</body>
+</html>
